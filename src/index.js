949cd34b77fff54c15ddbfea2e1ca119d4b03a1c
Aicha Dahlgren ElMansouri <152471871+aichadahlgren@users.noreply.github.com>
2024-10-02 14:00:13 +0200

    section 143

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-/* import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals'; */
-import StarRating from './StarRating';
+import './index.css';
+import App from './App-v1';
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+/* function Test() {
+  const [movieRating, setMovieRating] = useState(0); */
 
-  return (
+/*   return (
     <div>
       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movies was rated {movieRating} stars</p>
+      <p>This movies was rated aAicha {movieRating} stars</p>
     </div>
   );
-}
+} */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={2} />
-    {/*  //component */}
-
-    <Test />
+      onSetRating={() => { }}
+        />
+        <StarRating size={24} color="red" className="test" defaultRating={2} onSetRating={() => { 
+          }} />
+    <Test /> */}
   </React.StrictMode>
 );
